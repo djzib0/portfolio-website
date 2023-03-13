b1fb028497b1935f954e386f3871e4d3ed957300
djzib0 <piotrsabatura@gmail.com>
2023-03-13 15:36:21 +0100

    added sample data

---
+++ src/data/challengesData.js
@@ -0,0 +1,14 @@
+export default data = [
+    {
+        id: 1,
+        title: "challenge title",
+        description: "this is a challenge description",
+        solution: "this is a solution"
+    },
+    {
+        id: 2,
+        title: "challenge 2 title",
+        description: "this is a challenge 2 description",
+        solution: "this is a 2 solution"
+    }
+]
