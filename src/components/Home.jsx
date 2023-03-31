18a2d3f38416c589b3d8970e7e62edefbb657600
djzib0 <piotrsabatura@gmail.com>
2023-03-31 17:16:34 +0200

    update styles for small device for header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./home.css"
 import PORTRAIT from "../assets/images/portrait.jpg"
+import { GrFacebook } from "react-icons/gr"
 
 function Home() {
   return (
       <header>
         <div className='container header__container'>
-
           <div className='header__description'>
             <h1>Hi, I'm <span >Piotr</span></h1>
             <h5 className='weight-500'>Frontend <span>&</span> backend developer</h5>
@@ -18,7 +18,11 @@ function Home() {
           <div className='header__image'>
             <img src={PORTRAIT} alt="portrait picture" />
           </div>
-          dodać boczne odnośniki do social medias
+          {/* dodać boczne odnośniki do social medias */}
+
+          <div className="container socials__container">
+            <a href='https://facebook.com' target="_blank"><GrFacebook /></a>
+          </div>
         </div>
       </header>
   )
