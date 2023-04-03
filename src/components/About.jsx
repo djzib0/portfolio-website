08fd240c8bd812fd6f1a49a753348a97c85f1dc3
djzib0 <piotrsabatura@gmail.com>
2023-04-03 19:19:15 +0200

    added aboutme page and styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./about.css"
 
+import PORTRAIT from "../assets/images/portrait2.jpg"
+
 function About() {
   return (
-    <section className='about__container' id="about">About</section>
+    <section className='test-class' id="about">
+      <div className='about__container'>
+        <h1 className='about__header'>About <span className='border'>Me</span></h1>
+        <div className='about__container-description'>
+          <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur amet earum cupiditate qui nihil quis. Provident doloribus reiciendis veritatis unde, nobis nam, quis beatae recusandae expedita, quod molestias neque ullam?
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur amet earum cupiditate qui nihil quis. Provident doloribus reiciendis veritatis unde, nobis nam, quis beatae recusandae expedita, quod molestias neque ullam?
+          </h5>
+          <img src={ PORTRAIT } className='about__image'/>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
